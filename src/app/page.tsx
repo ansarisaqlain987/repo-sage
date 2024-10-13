dff13156c5c5bf9095843dbcd128e99683a95cd7
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-10-13 14:34:51 +0530

    added logic to generate name and some checkboxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,43 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { generateRepoName } from "@/utils/name.util";
-import { User2, RefreshCcw } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  GithubIcon,
+  RefreshIcon,
+  UserIcon,
+  CopyIcon,
+} from "@/components/icons";
 
-const GithubIcon = () => {
-  return (
-    <svg
-      role="img"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      width={24}
-      height={24}
-      fill="currentColor"
-      stroke="currentColor"
-    >
-      <title>GitHub</title>
-      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-    </svg>
-  );
-};
 export default function Component() {
   const [repoName, setRepoName] = useState<string>("");
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(true);
+  const [randomize, setRandomize] = useState<boolean>(false);
 
-  const generateAndSetName = () => {
-    setRepoName(generateRepoName());
+  const generateAndSetName = useCallback(() => {
+    setRepoName(
+      generateRepoName({
+        includeNumbers,
+        randomize,
+      })
+    );
+  }, [includeNumbers, randomize]);
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(repoName)
+      .then(() => {
+        console.log("copied to clipboard");
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
   };
+
   useEffect(() => {
     generateAndSetName();
-  }, []);
+  }, [generateAndSetName]);
 
   const handleGenerateName = () => {
     generateAndSetName();
@@ -37,7 +46,7 @@ export default function Component() {
 
   return (
     <div className="flex flex-col h-screen w-screen text-white">
-      <div className="flex-1 flex flex-col w-full">
+      <div className="flex-1 flex flex-col ">
         {/* Content */}
         <main className="flex-1 overflow-auto p-6 flex flex-col items-center justify-center">
           <h1 className="text-4xl font-bold mb-2">Name Generator</h1>
@@ -45,19 +54,58 @@ export default function Component() {
             Stand out with names as unique as your vision.
           </p>
           <div className="w-full max-w-2xl flex items-center gap-2">
-            <Input
-              className="w-full bg-gray-900 border-gray-700 rounded-md"
-              placeholder="Describe your project"
-              disabled
-              value={repoName}
-            />
-            <Button
-              className=" bg-[#10b981] hover:bg-[#0d9668]"
-              size="icon"
-              onClick={handleGenerateName}
-            >
-              <RefreshCcw className="h-6 w-6" />
+            <div className="w-full flex bg-gray-900 border-gray-700 rounded-md">
+              <Input
+                className="  text-xl"
+                placeholder="Describe your project"
+                disabled
+                value={repoName}
+              />
+              {repoName !== "" && (
+                <Button size="icon" variant={"ghost"} onClick={copyToClipboard}>
+                  <CopyIcon className="h-4 w-4" />
+                </Button>
+              )}
+            </div>
+            <Button size="icon" onClick={handleGenerateName}>
+              <RefreshIcon className="h-6 w-6" />
             </Button>
+          </div>
+          <div className="space-y-4 mt-8">
+            <div className="items-top flex space-x-2">
+              <Checkbox
+                id="include-numbers"
+                checked={includeNumbers}
+                onCheckedChange={(checked) => {
+                  setIncludeNumbers(!!checked.valueOf());
+                }}
+              />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="include-numbers"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Include Numbers
+                </label>
+              </div>
+            </div>
+            <div className="items-top flex space-x-2">
+              <Checkbox
+                id="randomize"
+                checked={randomize}
+                onCheckedChange={(checked) => {
+                  setRandomize(!!checked.valueOf());
+                }}
+              />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="randomize"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Randomize
+                </label>
+              </div>
+            </div>
           </div>
         </main>
       </div>
@@ -67,7 +115,7 @@ export default function Component() {
             <GithubIcon />
           </a>
           <a href="#" className="hover:text-white">
-            <User2 />
+            <UserIcon />
           </a>
         </div>
       </footer>
--- src/utils/name.util.ts
+++ src/utils/name.util.ts
@@ -5,11 +5,39 @@ import techTerms from "../data/tech-term.json";
 const getRandomElement = (array: string[]) =>
   array[Math.floor(Math.random() * array.length)];
 
-export const generateRepoName = () => {
-  const adj = getRandomElement(adjectives);
-  const noun = getRandomElement(nouns);
-  const tech = getRandomElement(techTerms);
-  const randomNum = Math.floor(Math.random() * 1000);
+type Option = {
+  includeNumbers?: boolean;
+  customName?: string;
+  randomize?: boolean;
+};
+export const generateRepoName = (option: Option = {}) => {
+  const nameArray = [];
+  if (option.customName) {
+    nameArray.push(option.customName);
+  }
+  nameArray.push(getRandomElement(adjectives));
+  nameArray.push(getRandomElement(nouns));
+  nameArray.push(getRandomElement(techTerms));
+  if (option.includeNumbers) {
+    nameArray.push(Math.floor(Math.random() * 1000));
+  }
 
-  return `${adj}-${noun}-${tech}-${randomNum}`;
+  const name = nameArray.join("-");
+  if (option.randomize) {
+    return createShuffledString(name.toLowerCase());
+  }
+  return name.toLowerCase();
+};
+
+const shuffleArray = (array: string[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const createShuffledString = (str: string) => {
+  const shuffledWords = shuffleArray(str.split("-"));
+  return shuffledWords.join("-");
 };
